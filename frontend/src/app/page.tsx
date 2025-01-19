ecfcbd534607cdf001e690300271e96533713d57
paula-em-lafon <paula.em.lafon@gmail.com>
2025-01-19 15:06:48 -0600

    full book editing and creating

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,10 +1,12 @@
 'use client';
 
 import React, { useState, useEffect } from 'react';
+import { useRouter } from 'next/navigation';
 import BookList from '../components/BookList';
 import { Book, Author } from '../types';
 
 const HomePage: React.FC = () => {
+  const router = useRouter();
   const [books, setBooks] = useState<Book[]>([]);
   const [authors, setAuthors] = useState<Author[]>([]);
   const [selectedAuthor, setSelectedAuthor] = useState<number | null>(null);
@@ -27,7 +29,6 @@ const HomePage: React.FC = () => {
   return (
     <div className="container mx-auto p-6">
       <h1 className="text-4xl font-bold text-center mb-6">Yes Sir I Can Boogie</h1>
-
       <div className="mb-4">
         <select
           onChange={(e) => setSelectedAuthor(Number(e.target.value) || null)}
@@ -40,9 +41,17 @@ const HomePage: React.FC = () => {
             </option>
           ))}
         </select>
+        <div className='flex items-end'>
+          <button
+              onClick={() => router.push(`/new/`)}
+              className="mt-2 bg-blue-500 text-white px-4 py-2 rounded"
+            >
+              New Book
+            </button>
+          </div>
       </div>
 
-      <BookList books={filteredBooks} />
+      <BookList books={books} authors={authors} />
     </div>
   );
 };
--- frontend/src/components/BookList.tsx
+++ frontend/src/components/BookList.tsx
@@ -1,21 +1,29 @@
 import React from 'react';
 import { useRouter } from 'next/navigation';
-import { Book } from '../types';
+import { Book, Author } from '../types';
 
 interface BookListProps {
   books: Book[];
+  authors: Author[];
 }
 
-const BookList: React.FC<BookListProps> = ({ books }) => {
+const BookList: React.FC<BookListProps> = ({ books, authors }) => {
   const router = useRouter();
+
+  // Function to get author name by ID
+  const getAuthorName = (authorId: number) => {
+    const author = authors.find((a) => a.id === authorId);
+    return author ? author.author : 'Unknown Author';
+  };
 
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
       {books.map((book) => (
         <div key={book.id} className="p-4 border rounded shadow-md">
           <h2 className="text-2xl font-bold">{book.title}</h2>
+          <h3 className="text-gray-600">Author: {getAuthorName(book.author)}</h3>
           <p className="text-gray-600">Year: {book.year}</p>
-          <p className="text-sm">{book.status}</p>
+          <p className="text-sm">Status: {book.status}</p>
           <button
             onClick={() => router.push(`/edit/${book.id}`)}
             className="mt-2 bg-green-500 text-white px-4 py-2 rounded"
